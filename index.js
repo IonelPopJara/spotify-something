58edc89686cfae2a22198f6a7bd9ebce3084a352
ionel <ionelalexandru.pop@gmail.com>
2023-04-20 01:57:50 +0200

    Refactor index.js

--- index.js
+++ index.js
@@ -1,11 +1,84 @@
-const accessToken = 'BQBEP2yWa6d8kKgsY9-Eyv47IpcGu0F3ELe1ypXHXz9ivfO7y0AfqGTgrqbPHyheBQ6BJxh1AomJuSCnJc-YuoLhSBIG0YNdpC0LSBPbWShd__-h2WO_EmmP5u3EMJon3uDVli9ShLt4EukeBjpd3g75XwaAK-Ni2iRuDC4RTE9u_2BREiHGUlD-KJTc7mNzzI7ruW8x_s43PEk';
+var accessToken;
+var expirationTime;
 
 const queryString = require('node:querystring');
+const dotenv = require('dotenv');
 const axios = require('axios');
 const express = require('express');
-const app = express();
 
-require('dotenv').config();
+const app = express();
+dotenv.config();
+
+const authenticateSpotify = async (code) => {
+    try {
+        const spotifyResponse = await axios.post(
+            'https://accounts.spotify.com/api/token',
+            queryString.stringify({
+                grant_type: 'authorization_code',
+                code,
+                redirect_uri: process.env.RED_URI_DEC,
+            }),
+            {
+                headers: {
+                    Authorization: "Basic " + process.env.BASE64_AUTH,
+                    'Content-Type': "application/x-www-form-urlencoded",
+                },
+            }
+        );
+
+        const {access_token, expires_in} = spotifyResponse.data;
+        var acc_token = access_token;
+        var expiration_time = Date.now() + expires_in * 1000;
+
+        return {
+            access_token : acc_token,
+            expiration_time : expiration_time
+        };
+    } catch (error) {
+        console.error(`Error refreshing token: ${error.message}`);
+    }
+    
+}
+
+const refreshToken = async () => {
+    try {
+        const spotifyResponse = await axios.post(
+            'https://accounts.spotify.com/api/token',
+            queryString.stringify({
+                grant_type: 'refresh_token',
+                refresh_token: refreshToken,
+            }),
+            {
+                headers: {
+                    Authorization: "Basic " + process.env.BASE64_AUTH,
+                    'Content-Type': "application/x-www-form-urlencoded",
+                },
+            }
+        );
+    
+        const {access_token, expires_in} = spotifyResponse.data;
+        accessToken = access_token;
+        expirationTime = Date.now() + expires_in * 1000;
+        console.log(`Access token refreshed: ${accessToken}`);
+    } catch (error) {
+        console.error(`Error refreshing token: ${error.message}`);
+    }
+    
+}
+
+const checkTokenValidity = async (req, res, next) => {
+    // Check if there is any access token
+    if (!accessToken) {
+        return res.status(401).json({message:'Access token is missing or invalid'});
+    }
+
+    // Check if the token has expired
+    if (Date.now() > expirationTime) {
+        await refreshToken();
+    }
+
+    next();
+}
 
 app.get('/', (req, res) => {
     res.send(
@@ -16,25 +89,44 @@ app.get('/', (req, res) => {
 });
 
 app.get('/account', async (req, res) => {
-    const spotifyResponse = await axios.post(
-        'https://accounts.spotify.com/api/token',
-        queryString.stringify({
-            grant_type: 'authorization_code',
-            code: req.query.code,
-            redirect_uri: process.env.RED_URI_DEC,
-        }),
-        {
-            headers: {
-                Authorization: "Basic " + process.env.BASE64_AUTH,
-                'Content-Type': "application/x-www-form-urlencoded",
-            },
-        }
-    );
+    try {
+        const {code} = req.query;
+        const {access_token, expiration_time} = await authenticateSpotify(code);
+        console.log(`Access token received: ${access_token}`);
 
-    console.log(spotifyResponse.data);
+        accessToken = access_token;
+        expirationTime = expiration_time;
+
+        res.redirect('/top');
+    } catch (error) {
+        console.error(`Error authenticating with Spotify: ${error.message}`);
+        res.status(500).send('Error authenticating with Spotify');
+    }
+    // const spotifyResponse = await axios.post(
+    //     'https://accounts.spotify.com/api/token',
+    //     queryString.stringify({
+    //         grant_type: 'authorization_code',
+    //         code: req.query.code,
+    //         redirect_uri: process.env.RED_URI_DEC,
+    //     }),
+    //     {
+    //         headers: {
+    //             Authorization: "Basic " + process.env.BASE64_AUTH,
+    //             'Content-Type': "application/x-www-form-urlencoded",
+    //         },
+    //     }
+    // );
+
+    // // accessToken = spotifyResponse.data.access_token;
+    // const {access_token, expires_in} = spotifyResponse.data;
+    // accessToken = access_token;
+    // expirationTime = Date.now() + expires_in * 1000;
+    // // console.log('Access Token: ' + accessToken + '\n');
+    // // console.log('Current Time: ' + Date.now() + '\n');
+    // // console.log('Expiration Time: ' + expirationTime + '\n');
 });
 
-app.get('/top', async (req, res) => {
+app.get('/top', checkTokenValidity ,async (req, res) => {
     try {
         const response = await axios.get('https://api.spotify.com/v1/me/top/tracks', {
             params: {
@@ -46,9 +138,19 @@ app.get('/top', async (req, res) => {
             },
         });
 
-        console.log(response.data);
+        const tracks = await Promise.all(response.data.items.map(async (item) => {
+            const audio = await axios.get(`https://api.spotify.com/v1/audio-features/${item.id}`, {
+                headers: {
+                    Authorization: `Bearer ${accessToken}`,
+                },
+            });
 
-        const tracks = response.data.items.map((item) => {
+            const artistResponse = await axios.get(`https://api.spotify.com/v1/artists/${item.artists[0].id}`, {
+                headers: {
+                    Authorization: `Bearer ${accessToken}`,
+                },
+            });
+
             return {
                 name: item.name,
                 artist: item.artists.map((artist) => artist.name).join(', '),
@@ -56,8 +158,16 @@ app.get('/top', async (req, res) => {
                 preview_url: item.preview_url,
                 explicit : item.explicit,
                 popularity: item.popularity,
+                id: item.id,
+                acousticness: audio.data.acousticness,
+                key: audio.data.key,
+                danceability: audio.data.danceability,
+                liveness: audio.data.liveness,
+                instrumentalness: audio.data.instrumentalness,
+                energy: audio.data.energy,
+                genres: artistResponse.data.genres,
             };
-        });
+        }));
 
         res.send(tracks);
     } catch (error) {
